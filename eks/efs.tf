ea489f21c0a3c7335d5982a0ccdd4a6301f6129e
Jose Ramon Mañes <jose@celestia.org>
2023-03-21 12:17:10 +0100

    feature: EFS WORKING! love it :)

--- eks/efs.tf
+++ eks/efs.tf
@@ -1,31 +1,55 @@
 # EFS for Testground outputs
-resource "aws_efs_file_system" "default" {
-  count           = 1
+locals {
+  azs = ["${local.region}a", "${local.region}b", "${local.region}c"]
 
-  tags = {
+  name = "${local.project_name}-${local.environment}"
+}
+
+module "efs" {
+  source  = "terraform-aws-modules/efs/aws"
+  version = "~> 1.0"
+
+  creation_token = "${local.project_name}-${local.environment}-efs"
+  name           = "${local.project_name}-${local.environment}-efs"
+
+
+  # Mount targets / security group
+  mount_targets = {
+    for k, v in zipmap(local.azs, module.vpc.private_subnets) : k => { subnet_id = v }
+  }
+  security_group_description = "${local.name} EFS security group"
+  security_group_vpc_id      = module.vpc.vpc_id 
+  security_group_rules = {
+    vpc = {
+      # relying on the defaults provdied for EFS/NFS (2049/TCP + ingress)
+      description = "NFS ingress from VPC private subnets"
+      cidr_blocks = module.vpc.private_subnets_cidr_blocks
+    }
+  }
+
+    tags = {
     Name = "${local.project_name}-${local.environment}-efs"
   }
 }
 
-// TODO: refactor this part PLEASE
-resource "aws_efs_mount_target" "target_subnet_zone_a" {
-  count           = 1
-  file_system_id  = join("", aws_efs_file_system.default.*.id)
-  subnet_id       = module.vpc.private_subnets[0]
-  security_groups = [module.vpc.default_security_group_id]
+resource "kubernetes_storage_class_v1" "efs" {
+  metadata {
+    name = "aws-efs"
+  }
+
+  storage_provisioner = "efs.csi.aws.com"
+  parameters = {
+    provisioningMode = "efs-ap" # Dynamic provisioning
+    fileSystemId     = module.efs.id
+    directoryPerms   = "700"
+  }
+
+  mount_options = [
+    "iam"
+  ]
+
+  depends_on = [
+    module.eks_blueprints_kubernetes_addons
+  ]
 }
 
-resource "aws_efs_mount_target" "target_subnet_zone_b" {
-  count           = 1
-  file_system_id  = join("", aws_efs_file_system.default.*.id)
-  subnet_id       = module.vpc.private_subnets[1]
-  security_groups = [module.vpc.default_security_group_id]
-}
-
-
-resource "aws_efs_mount_target" "target_subnet_zone_c" {
-  count           = 1
-  file_system_id  = join("", aws_efs_file_system.default.*.id)
-  subnet_id       = module.vpc.private_subnets[2]
-  security_groups = [module.vpc.default_security_group_id]
-}
